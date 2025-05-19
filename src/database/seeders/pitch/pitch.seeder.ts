738787af4b0f09b54de089fccdadad09cafce8df
khuoy borin satya <khuoyborin@gmail.com>
2025-05-19 16:56:08 +0700

    seed pitch and sport

--- src/database/seeders/pitch/pitch.seeder.ts
+++ src/database/seeders/pitch/pitch.seeder.ts
@@ -1,10 +1,61 @@
+import Pitches from "../../../models/pitch/pitches.model";
+import PitchesCategory from "../../../models/pitch/pitches_category.model";
+const now = new Date();
 export class PitchSeeder {
 
     seed = async () => {
-
+        try {
+            await PitchesCategory.bulkCreate(pitchSeeder.pitch_categories);
+            console.log('\x1b[32m\nSeed pitch category inserted successfully.');
+        } catch (error) {
+            console.error('Error seeding orders:', error);
+        }
+        try {
+            await Pitches.bulkCreate(pitchSeeder.pitch);
+            console.log('\x1b[32m\nSeed pitch inserted successfully.');
+        } catch (error) {
+            console.error('Error seeding orders:', error);
+        }
+        
     }
 }
 
-const data = {
+const pitchSeeder = {
+   pitch_categories: [
+        {
+            id: 1,
+            sport_id: 1,
+            name: "Standard 5v5",
+            required_players: 10,
+            volume: 300,
+            price: 20.0,
+            created_at: now,
+            updated_at: now,
+        },
+        {
+            id: 2,
+            sport_id: 2,
+            name: "Half Court",
+            required_players: 6,
+            volume: 150,
+            price: 15.0,
+            created_at: now,
+            updated_at: now,
+        }
+   ],
+    pitch: [
+        {
+            category_id: 1,
+            name: "Football Pitch A",
+            created_at: now,
+            updated_at: now,
+        },
+        {
+            category_id: 2,
+            name: "Basketball Court 1",
+            created_at: now,
+            updated_at: now,
+        }
+    ]
 
 };
--- src/database/seeders/sport/sport.seeder.ts
+++ src/database/seeders/sport/sport.seeder.ts
@@ -1,10 +1,67 @@
+import SportRating from "../../../models/sport/sport_ratings.model";
+import Sports from "../../../models/sport/sports.model";
+
+// import SportRating from "src/models/sport/sport_ratings.model";
+const now = new Date();
+
 export class SportSeeder {
 
     seed = async () => {
-
+        try {
+            await Sports.bulkCreate(sportSeeder.sport);
+            console.log('\x1b[32m\nSeed sport inserted successfully.');
+        } catch (error) {
+            console.error('Error seeding orders:', error);
+        }
+        try {
+            await SportRating.bulkCreate(sportSeeder.sport_ratings);
+            console.log('\x1b[32m\nSeed sport rating inserted successfully.');
+        } catch (error) {
+            console.error('Error seeding orders:', error);
+        }
+      
     }
 }
 
-const data = {
-
+const sportSeeder = {
+    sport: [
+        {
+            name : "Football",
+            created_at : now,
+            updated_at : now
+        },
+        {
+            name : "Basketball",
+            created_at : now,
+            updated_at : now
+        },
+        {
+            name : "Tennis",
+            created_at : now,
+            updated_at : now
+        }
+    ],
+    sport_ratings: [
+        {
+            sport_id : 1,
+            rating : 5,
+            description: 'Widely loved worldwide.',
+            created_at: now,
+            updated_at: now
+        },
+         {
+            sport_id : 2,
+            rating : 3,
+            description: 'Widely loved worldwide.',
+            created_at: now,
+            updated_at: now
+        },
+         {
+            sport_id : 3,
+            rating : 4,
+            description: 'Widely loved worldwide.',
+            created_at: now,
+            updated_at: now
+        }
+    ]
 };
