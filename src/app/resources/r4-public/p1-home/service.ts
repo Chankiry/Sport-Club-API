90c5e8e9803929d7d7f590c1aedc9daa854be346
kimchamraoen <kimchamraoen@gmail.com>
2025-06-11 16:13:00 +0700

    listing equipments

--- src/app/resources/r4-public/p1-home/service.ts
+++ src/app/resources/r4-public/p1-home/service.ts
@@ -6,6 +6,8 @@ import { Op, Sequelize } from 'sequelize';
 import * as moment from 'moment';
 import User from 'src/models/user/user.model';
 
+const IMAGE_URL = 'http://localhost:5173';
+
 // ===========================================================================>> Custom Library
 
 // ===>> Model
@@ -20,47 +22,47 @@ export class PublicHomeService {
           football_equipments: [
             {
               id: 1,
-              image: '../assets/images/shoes6.avif',
-              name: 'aadi aadi Trendy Lightweight Sports',
+              image: `${IMAGE_URL}/images/soccer1.jpg`,
+              name: 'aadi Trendy Lightweight Sports',
               description:
                 'The shoes feature a breathable mesh outer material that allows air circulation, keeping your feet cool and dry during intense workout sessions',
               price: 270,
             },
             {
               id: 2,
-              image: '../assets/images/ball1.webp',
-              name: 'adidas Fussballliebe Competition Ball',
+              image: `${IMAGE_URL}/images/ball1.webp`,
+              name: 'adidas Fussballliebe',
               description:
                 'The adidas Fussballliebe Competition Ball is a high-quality football designed for competitive play. It features a durable construction and advanced technology to ensure optimal performance on the field.',
               price: 130,
             },
             {
               id: 3,
-              image: '../assets/images/uniform6.jpg',
-              name: 'China Manufacture Polyester Football Sports',
+              image: `${IMAGE_URL}/images/uniform1.avif`,
+              name: 'China Manufacture Polyester',
               description:
                 'China Manufacture Polyester Football Sports is a high-quality football made from durable polyester material, designed for both training and competitive play. It offers excellent performance and longevity on the field.',
               price: 70,
             },
             {
               id: 4,
-              image: '../assets/images/shoes7.avif',
-              name: 'Nike Mercurial Vapor 16 Elite x Air Max 95 SE',
+              image: `${IMAGE_URL}/images/soccer2.webp`,
+              name: 'Nike Mercurial Vapor 16',
               description:
                 'As we celebrate the 30th anniversary of the Air Max 95, it is only right we toast the rarefied AIR allegiance between the lifestyle icon and meteoric Mercurial.',
               price: 270,
             },
             {
               id: 5,
-              image: '../assets/images/ball1.webp',
-              name: 'Paris Saint-Germain Strike Third',
+              image: `${IMAGE_URL}/images/ball2.jpeg`,
+              name: 'Paris Saint-Germain',
               description:
                 'With design details specifically tailored for football is rising stars, a slim, streamlined fit ensures that nothing comes between you and the ball.',
               price: 130,
             },
             {
               id: 6,
-              image: '../assets/images/uniform6.jpg',
+              image: `${IMAGE_URL}/images/uniform2.avif`,
               name: 'Nike Academy',
               description:
                 'Optimise dribbling with textured casing, visibility with high-contrast graphics and accuracy with moulded grooves so you can focus on the game not the equipment.',
@@ -70,25 +72,23 @@ export class PublicHomeService {
           volleyball_equipments: [
             {
               id: 1,
-              image: '../assets/images/soccer2.webp',
-              name: 'Latest Stylish Casual Sneaker Sports ',
+              image: `${IMAGE_URL}/images/shoes1.webp`,
+              name: 'Latest Stylish Casual Sneaker',
               description:
                 'Sneakers are made for exercise and sports, but they are also very popular everyday shoes because they are so comfortable',
-
               price: 180,
             },
             {
               id: 2,
-              image: '../assets/images/ball2.jpeg',
+              image: `${IMAGE_URL}/images/ball3.webp`,
               name: 'Cheap Ball Games',
               description:
                 'Cheap Ball Games is a budget-friendly option for sports enthusiasts looking for affordable yet quality balls for various games. These balls are designed to provide good performance without breaking the bank.',
-
               price: 120,
             },
             {
               id: 3,
-              image: '../assets/images/uniform11.webp',
+              image: `${IMAGE_URL}/images/shoes2.avif`,
               name: 'HACKY SPORTS',
               description:
                 'HACKY SPORTS is a brand known for its high-quality sports apparel and equipment. They offer a wide range of products designed for various sports, ensuring comfort and performance for athletes.',
@@ -96,7 +96,7 @@ export class PublicHomeService {
             },
             {
               id: 4,
-              image: '../assets/images/shoes6.avif',
+              image: `${IMAGE_URL}/images/uniform9.webp`,
               name: 'Nike P-6000',
               description:
                 'The Nike P-6000 draws on the 2006 Nike Air Pegasus, bringing you a mash-up of iconic style that is breathable, comfortable and evocative of that early-2000s vibe.',
@@ -104,7 +104,7 @@ export class PublicHomeService {
             },
             {
               id: 5,
-              image: '../assets/images/ball1.webp',
+              image: `${IMAGE_URL}/images/uniform6.jpg`,
               name: 'Nike Strike',
               description:
                 'On the pitch, passing, dribbling or scoring, this lightweight top is designed to work as hard as you do. With design details specifically tailored for football is rising stars, a classic fit helps limit distractions when you are on the pitch.',
@@ -112,7 +112,7 @@ export class PublicHomeService {
             },
             {
               id: 6,
-              image: '../assets/images/uniform6.jpg',
+              image: `${IMAGE_URL}/images/ball4.jpg`,
               name: 'Nike Pitch',
               description:
                 'Kick off your love for the game with the Nike Pitch Football. Made for your beginner training sessions and enhancing your footwork, this ball features a durable casing to help maintain consistent performance.',
@@ -122,7 +122,7 @@ export class PublicHomeService {
           tennis_equipments: [
             {
               id: 1,
-              image: '../assets/images/soccer1.jpg',
+              image: `${IMAGE_URL}/images/shoes7.webp`,
               name: 'DRACKFOOT Sport Shoe',
               description:
                 'DRACKFOOT Sport Shoe is a versatile and stylish footwear option designed for both athletic performance and casual wear. These shoes are crafted with a focus on comfort, durability, and modern aesthetics.',
@@ -130,15 +130,15 @@ export class PublicHomeService {
             },
             {
               id: 2,
-              image: '../assets/images/table2.webp',
-              name: 'MAPOL 200 Count Table Tennis Balls',
+              image: `${IMAGE_URL}/images/table1.webp`,
+              name: 'MAPOL 200 Count Table Tennis',
               description:
                 'MAPOL 200 Count Table Tennis Balls are high-quality, durable balls designed for table tennis enthusiasts. These balls are perfect for both recreational and competitive play, offering consistent bounce and performance.',
               price: 85,
             },
             {
               id: 3,
-              image: '../assets/images/uniform10.webp',
+              image: `${IMAGE_URL}/images/uniform11.webp`,
               name: 'Soccer Uniform',
               description:
                 'Soccer Uniform is a complete set of clothing designed for soccer players, including jerseys, shorts, and socks. Made from breathable and moisture-wicking materials, it ensures comfort and performance on the field.',
@@ -146,7 +146,7 @@ export class PublicHomeService {
             },
             {
               id: 4,
-              image: '../assets/images/shoes6.avif',
+              image: `${IMAGE_URL}/images/shoes6.webp`,
               name: 'Nike Air Force 1 Flyknit 2.0',
               description:
                 'Inspired by the shoe that is been reigning the courts since 1982, the Nike Air Force 1 Flyknit 2.0 brings back the AF-1 in a lighter-than-ever design.Rocking Flyknit construction with classic AF-1 design lines, it serves up old-school hoops vibes with a featherweight feel.',
@@ -154,15 +154,15 @@ export class PublicHomeService {
             },
             {
               id: 5,
-              image: '../assets/images/ball1.webp',
-              name: 'Paris Saint-Germain 2024/25 Stadium Away',
+              image: `${IMAGE_URL}/images/uniform10.webp`,
+              name: 'Paris Saint-Germain',
               description:
                 'Outfit future little stars in full with this three-piece kit that includes a team shirt with matching shorts and socks. Inspired by what the pros wear during play, our Stadium Collection has lightweight, sweat-wicking fabric to help keep young athletes comfortable while they zoom, scurry or play.',
               price: 130,
             },
             {
               id: 6,
-              image: '../assets/images/uniform6.jpg',
+              image: `${IMAGE_URL}/images/table2.webp`,
               name: 'The Official Table Tennis Table',
               description:
                 'MAPOL 300 Count Table Tennis Balls are high-quality, durable balls designed for table tennis enthusiasts. These balls are perfect for both recreational and competitive play, offering consistent bounce and performance.',
